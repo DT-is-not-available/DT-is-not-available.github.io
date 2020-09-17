2c947120594bd240fd84dbac303f8617fd58ab6f
TheDT365 <66632141+GDEngine@users.noreply.github.com>
2020-09-17 15:03:02 -0400

    Add files via upload

---
+++ unfairmario/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1600369268,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/unvairlogo-sheet0.png",
+		"images/map.png",
+		"images/mario-sheet0.png",
+		"images/marioimage.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/spawner-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
