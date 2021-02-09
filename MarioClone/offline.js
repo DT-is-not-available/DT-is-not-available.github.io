e651f616360f15c4b4a1fee53a4aca52fadf39e8
TheDT365 <66632141+GDEngine@users.noreply.github.com>
2021-02-08 20:49:22 -0500

    Add files via upload

--- MarioClone/offline.js
+++ MarioClone/offline.js
@@ -1,14 +1,16 @@
 ﻿{
-	"version": 1612817156,
+	"version": 1612835286,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/tilemap.png",
+		"images/collision.png",
 		"images/player-sheet0.png",
 		"images/baddie-sheet0.png",
 		"images/baddie-sheet1.png",
+		"images/spawner-sheet0.png",
+		"images/texture.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
