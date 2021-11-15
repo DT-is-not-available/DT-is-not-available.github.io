50474afb96ac538abe996068479372bddedeb18b
DT <66632141+GDEngine@users.noreply.github.com>
2021-11-15 07:52:27 -0500

    f

--- epicadventure-modder/main.js
+++ epicadventure-modder/main.js
@@ -41,9 +41,27 @@ function load() {
 		Result.innerHTML = JSON.stringify(data)
 	})
 	
+	width = document.getElementById("width")
+	width.addEventListener('change', function(){
+		data.project[10] = width.value
+		Result.innerHTML = JSON.stringify(data)
+	})
+	
+	height = document.getElementById("height")
+	height.addEventListener('change', function(){
+		data.project[11] = height.value
+		Result.innerHTML = JSON.stringify(data)
+	})
+	
 	shop1 = document.getElementById("shop1")
 	shop1.addEventListener('change', function(){
 		data.project[5][4][6][0][14][5][3][0][0] = shop1.value.replace(/\n/g, ";");
+		Result.innerHTML = JSON.stringify(data)
+	})
+	
+	shop1talk = document.getElementById("shop1talk")
+	shop1talk.addEventListener('change', function(){
+		data.project[5][4][6][0][14][4][3][0][0] = shop1talk.value
 		Result.innerHTML = JSON.stringify(data)
 	})
 	
@@ -53,9 +71,21 @@ function load() {
 		Result.innerHTML = JSON.stringify(data)
 	})
 	
+	shop2talk = document.getElementById("shop2talk")
+	shop2talk.addEventListener('change', function(){
+		data.project[5][5][6][0][14][4][3][0][0] = shop2talk.value
+		Result.innerHTML = JSON.stringify(data)
+	})
+	
 	shop3 = document.getElementById("shop3")
 	shop3.addEventListener('change', function(){
 		data.project[5][6][6][0][14][5][3][0][0] = shop3.value.replace(/\n/g, ";");
+		Result.innerHTML = JSON.stringify(data)
+	})
+	
+	shop3talk = document.getElementById("shop3talk")
+	shop3talk.addEventListener('change', function(){
+		data.project[5][6][6][0][14][4][3][0][0] = shop3talk.value
 		Result.innerHTML = JSON.stringify(data)
 	})
 }
