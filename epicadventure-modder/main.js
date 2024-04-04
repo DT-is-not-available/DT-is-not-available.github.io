d603165ad529a86c97598199582aa66df4b9a60b
DT <66632141+DT-is-not-available@users.noreply.github.com>
2024-04-04 13:06:24 -0400

    Update main.js

--- epicadventure-modder/main.js
+++ epicadventure-modder/main.js
@@ -268,91 +268,92 @@ function load() {
 			project = verbosify(data.project, projectTemplate, true)
 		}
 	}
-	xhttp.open("GET", "./default-data.json", true)
+	xhttp.open("GET", "./play/data.js", true)
 	xhttp.send()
 
 	Result = document.getElementById("result")
 	
 	gameTitle = document.getElementById("gameTitle")
 	gameTitle.addEventListener('change', function(){
-		data.project[5][1][6][1][14][0][3][0][0] = gameTitle.value.toUpperCase()
-		data.project[5][1][6][1][14][1][3][0][0] = gameTitle.value
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		project.layouts[1].layers[1].objects[0].instanceVariables[0][0] = gameTitle.value.toUpperCase()
+		project.layouts[1].layers[1].objects[1].instanceVariables[0][0] = gameTitle.value
 	})
 	
 	gameTitleAbove = document.getElementById("gameTitleAbove")
 	gameTitleAbove.addEventListener('change', function(){
-		data.project[5][1][6][1][14][2][5][3] = gameTitleAbove.value
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		project.layouts[1].layers[1].objects[2].properties[3] = gameTitleAbove.value
 	})
 	
 	gameTitleBelow = document.getElementById("gameTitleBelow")
 	gameTitleBelow.addEventListener('change', function(){
-		data.project[5][1][6][1][14][4][5][3] = gameTitleBelow.value
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		project.layouts[1].layers[1].objects[4].properties[3] = gameTitleBelow.value
 	})
 	
 	gameTitleBelow = document.getElementById("gameTitleBelowShadow")
 	gameTitleBelow.addEventListener('change', function(){
-		data.project[5][1][6][1][14][3][5][3] = gameTitleBelow.value
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		project.layouts[1].layers[1].objects[3].properties[3] = gameTitleBelow.value
 	})
 	
 	startingLayout = document.getElementById("Starting Layout")
 	startingLayout.addEventListener('change', function(){
-		data.project[1] = startingLayout.value
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		project.startingLayout = startingLayout.value
 	})
 	
 	width = document.getElementById("width")
 	width.addEventListener('change', function(){
-		data.project[10] = width.value
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		project.viewportWidth = width.value
 	})
 	
 	height = document.getElementById("height")
 	height.addEventListener('change', function(){
-		data.project[11] = height.value
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		project.viewportHeight = height.value
 	})
 	
 	shop1 = document.getElementById("shop1")
 	shop1.addEventListener('change', function(){
-		data.project[5][4][6][0][14][5][3][0][0] = shop1.value.replace(/\n/g, ";");
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		data.project.layouts[4].layers[0].objects[5].instanceVariables[0][0] = shop1.value.replace(/\n/g, ";");
 	})
 	
 	shop1talk = document.getElementById("shop1talk")
 	shop1talk.addEventListener('change', function(){
-		data.project[5][4][6][0][14][4][3][0][0] = shop1talk.value
+		document.getElementById("result").innerText = "please recompile"
+		data.project.layouts[4].layers[0].objects[4].instanceVariables[0][0] = shop1talk.value
 		Result.innerHTML = JSON.stringify(data)
 	})
 	
 	shop2 = document.getElementById("shop2")
 	shop2.addEventListener('change', function(){
-		data.project[5][5][6][0][14][5][3][0][0] = shop2.value.replace(/\n/g, ";");
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		data.project.layouts[5].layers[0].objects[5].instanceVariables[0][0] = shop2.value.replace(/\n/g, ";");
 	})
 	
 	shop2talk = document.getElementById("shop2talk")
 	shop2talk.addEventListener('change', function(){
-		data.project[5][5][6][0][14][4][3][0][0] = shop2talk.value
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		data.project.layouts[5].layers[0].objects[4].instanceVariables[0][0] = shop2talk.value
 	})
 	
 	shop3 = document.getElementById("shop3")
 	shop3.addEventListener('change', function(){
-		data.project[5][6][6][0][14][5][3][0][0] = shop3.value.replace(/\n/g, ";");
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		data.project.layouts[6].layers[0].objects[5].instanceVariables[0] = shop3.value.replace(/\n/g, ";");
 	})
 	
 	shop3talk = document.getElementById("shop3talk")
 	shop3talk.addEventListener('change', function(){
-		data.project[5][6][6][0][14][4][3][0][0] = shop3talk.value
-		Result.innerHTML = JSON.stringify(data)
+		document.getElementById("result").innerText = "please recompile"
+		data.project.layouts[6].layers[0].objects[4].instanceVariables[0] = shop3talk.value
 	})
 }
 function playMod() {
-	alert("Coming Soon!")
+	document.getElementById("result").innerText = JSON.stringify(data)
 }
 window.onload = load
